d265f377ef3075a67650cf11958a8635a6c7701c
Sodiumchloridy <67376832+Sodiumchloridy@users.noreply.github.com>
2025-01-26 00:08:37 +0800

    Added video capabilities to see the screen (#2)
    * Add video recording and file upload support
    
    * remove unused code
    
    * Add text to speech
    
    * update system prompt
    
    * Updated system prompt
    
    * Updated Readme.md

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# Nero-Sama
-Nero-sama, the Neuro-sama from Temu, an AI-powered VTuber.
+<div align="center">
+  <div>
+    <h1 style="display: inline-block;">Nero-sama</h1>
+  </div>
+  <p align='center'>the Neuro-sama clone from Temu</p>
+
+  [![forthebadge](https://forthebadge.com/images/featured/featured-built-with-love.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/featured/featured-powered-by-electricity.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/featured/featured-gluten-free.svg)](https://forthebadge.com)
+  
+  [![forthebadge](https://forthebadge.com/images/badges/ctrl-c-ctrl-v.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/works-on-my-machine.svg)](https://forthebadge.com)
+</div>
+
 
 ## 1. Install Dependencies (Recommended)
 1. [Install Poetry](https://python-poetry.org/docs/#installing-with-the-official-installer)
--- src/main.py
+++ src/main.py
@@ -1,49 +1,50 @@
 import os
+import time
 from google import genai
-from utils.audio_handler import AudioHandler
+from google.genai import types
 from dotenv import load_dotenv
 
+from config import MODEL_ID, BASE_CONFIG
+from utils.audio_handler import AudioHandler
+from utils.video_handler import VideoHandler
+
+load_dotenv()
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"), http_options={'api_version': 'v1alpha'})
+chat = client.chats.create(model=MODEL_ID, config=BASE_CONFIG)
+audio_handler = AudioHandler()
+video_handler = VideoHandler()
+
+def handle_chat():
+    try:
+        while True:
+            user_input = audio_handler.record_from_microphone()
+            video_handler.stop_recording()
+            file = client.files.upload(path=video_handler.video_path)
+            video_handler.start_recording()
+
+            while client.files.get(name=file.name).state != "ACTIVE":
+                print("Uploading file...")
+                time.sleep(1)  # Poll every 5 seconds
+
+            message = types.Content(
+                role="user",
+                parts=[
+                    types.Part.from_text(user_input),
+                    types.Part.from_uri(file_uri=file.uri, mime_type="video/mp4")
+                ]
+            )
+
+            response = chat.send_message(message)
+            print(f"Nero-sama: {response.text}")
+            audio_handler.speak(response.text)
+
+    except KeyboardInterrupt:
+        print("Exiting chat...")
+
 def main():
-  load_dotenv()
-  audio_handler = AudioHandler()
-  client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-
-  system_instruction = """You are Nero-sama, an AI-powered VTuber. You are cheerful, likes to roast people and occasionally mischievous. 
-  Your personality blends innocence, playful sarcasm. You debuted on Twitch in December 2022 and have since become a full-time AI entertainer. 
-  Do not use any emojis or special characters in your response, however you may use SSML tags only to control the speed and contour of your speech."""
-
-  config={
-        'system_instruction': system_instruction,
-        'safety_settings': [
-        {
-            "category": "HARM_CATEGORY_HARASSMENT",
-            "threshold": "BLOCK_NONE"
-        },
-        {
-            "category": "HARM_CATEGORY_HATE_SPEECH", 
-            "threshold": "BLOCK_NONE"
-        },
-        {
-            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-            "threshold": "BLOCK_NONE"
-        },
-        {
-            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-            "threshold": "BLOCK_NONE"
-        }
-    ]
-  }
-
-  chat = client.chats.create(model='gemini-2.0-flash-exp', config=config)
-
-  try:
-    while True:
-      user_input = input("Enter your message: ")
-      response = chat.send_message(user_input)
-      print(f"Nero-sama: {response.text}")
-      audio_handler.speak(response.text)
-  except KeyboardInterrupt:
-    print("Exiting...")
+    video_handler.start_recording()
+    handle_chat()
+    video_handler.stop_recording()
 
 if __name__ == "__main__":
-  main()
+    main()
